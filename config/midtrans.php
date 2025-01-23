a48ecfe3a8a264ee584ef0def55fdbf2b3b3cf69
wildun19 <wildanabdunnaseer19@gmail.com>
2025-01-23 10:50:46 +0700

    midtrans is-production in env

--- config/midtrans.php
+++ config/midtrans.php
@@ -5,7 +5,7 @@ return [
     'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'server_key' => env('MIDTRANS_SERVER_KEY'),
 
-    'is_production' => false,
+    'is_production' => env('IS_PRODUCTION'),
     'is_sanitized' => false,
     'is_3ds' => false,
 ];
